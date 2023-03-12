aaf1537100e1b37c7833b075a3e1642b10ff7221
Le Ngoc Tien <94373805+ngoctienle@users.noreply.github.com>
2023-03-12 12:11:43 +0700

    feat(tawkto): implement Tawkto into repo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import type { AppProps } from 'next/app'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import { Fragment } from 'react'
 import { Toaster } from 'react-hot-toast'
 
@@ -38,6 +39,19 @@ export default function App({ Component, pageProps }: AppProps) {
             duration: 2500
           }}
         />
+        <Script id='tawk' strategy='lazyOnload'>
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/6406b5d84247f20fefe462fa/1gqt3clk5';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();  
+        `}
+        </Script>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </Fragment>
