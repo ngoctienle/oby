1be5eb1ea74f0cb4a9b649db2cdbdd9d4f9fb44c
ngoctienle <ngoctienle.2399@gmail.com>
2023-05-12 11:50:57 +0700

    fix(index): banner

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -13,34 +13,36 @@ export default function Banner() {
   const isMatch = useMediaQuery('(min-width:992px)')
 
   return isMatch ? (
-    <Swiper
-      effect={'fade'}
-      navigation={true}
-      lazy={{
-        loadPrevNext: true
-      }}
-      pagination={{
-        clickable: true
-      }}
-      modules={[EffectFade, Navigation, Pagination, Lazy]}
-      className='obyBanner'
-    >
-      {Array(4)
-        .fill(0)
-        .map((_, index) => (
-          <SwiperSlide key={index}>
-            <div className='relative w-full aspect-[16/6]'>
-              <OBYImage
-                quality={100}
-                alt='img'
-                src='/images/oby-banner.png'
-                display='responsive'
-                style={{ objectPosition: 'center', objectFit: 'cover' }}
-              />
-            </div>
-          </SwiperSlide>
-        ))}
-    </Swiper>
+    <div id='banner-oby' className='relative -z-50'>
+      <Swiper
+        effect={'fade'}
+        navigation={true}
+        lazy={{
+          loadPrevNext: true
+        }}
+        pagination={{
+          clickable: true
+        }}
+        modules={[EffectFade, Navigation, Pagination, Lazy]}
+        className='obyBanner'
+      >
+        {Array(4)
+          .fill(0)
+          .map((_, index) => (
+            <SwiperSlide key={index}>
+              <div className='relative w-full aspect-[16/6]'>
+                <OBYImage
+                  quality={100}
+                  alt='img'
+                  src='/images/oby-banner.png'
+                  display='responsive'
+                  style={{ objectPosition: 'center', objectFit: 'cover' }}
+                />
+              </div>
+            </SwiperSlide>
+          ))}
+      </Swiper>
+    </div>
   ) : (
     <div className='container'>
       <Swiper
