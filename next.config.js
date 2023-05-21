d1b4bdedf51595cc4ae49aa947616e6b1c604485
ngoctienle <ngoctienle.2399@gmail.com>
2023-05-21 15:52:55 +0700

    fix(url): update url api

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const isDev = process.env.NODE_ENV === 'development'
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['13.229.90.199', '13.229.90.199undefined'],
+    domains: ['13.229.90.199', '13.229.90.199undefined', 'api.ongbayeu.com', 'api.ongbayeu.comundefined'],
     minimumCacheTTL: 60
   },
   compiler: {
