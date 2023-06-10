da37f01d2d681952c2781ab2fb823da58787be97
kaylee182000 <trungkien40414@gmail.com>
2023-06-10 12:18:33 +0700

    fix(blog): fix UI blog broken

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -44,41 +44,49 @@ export default function BlogPage({ categories, initBlogs }: BlogPageProps) {
       <section className='@992:pt-4 pt-3'>
         <Breadcrumb cateName={'Blog'} />
         <div className='container'>
-          <div className='grid grid-cols-12 gap-10 min-h-[300px]'>
-            <div className='@992:col-span-3 @992:order-1 order-2 col-span-12 @992:mt-0 @992:pt-0 mt-6 pt-6 border-t border-t-oby-primary @992:border-t-transparent'>
-              <h2 className='text-oby-green fs-18 font-bold'>Tin tức theo danh mục</h2>
+          <div className='grid @992:grid-cols-12 grid-cols-1  gap-10 min-h-[300px]'>
+            <div className='@992:col-span-3 col-span-1'>
+              <h2 className='text-oby-green fs-18 font-bold'>Danh mục Blog</h2>
               <RadioGroup value={selectedCate} onChange={setSelectedCate}>
-                <div className='space-y-3.5 mt-4'>
-                  {categories.map((category) => {
-                    if (category.id !== 1) {
-                      return (
-                        <RadioGroup.Option
-                          key={category.name}
-                          value={category.id}
-                          className={({ checked }) =>
-                            `${checked ? 'bg-oby-primary' : 'bg-white'}
+                <div className='overflow-y-scroll scrollbar-none @992:bg-transparent @992:mt-0 mt-4 bg-[#BCF0AA] rounded-4  @992:p-0 py-2 px-4'>
+                  <div className='@992:space-y-3.5 space-x-2 @992:mt-4 @992:w-auto w-[992px] @992:block flex items-center'>
+                    {categories.map((category) => {
+                      if (category.id !== 1) {
+                        return (
+                          <RadioGroup.Option
+                            key={category.name}
+                            value={category.id}
+                            className={({ checked }) =>
+                              `${checked ? 'bg-oby-primary' : 'bg-white'}
                               relative flex cursor-pointer rounded-4 border border-oby-primary px-4 py-2.5 focus:outline-none`
-                          }
-                        >
-                          {({ checked }) => (
-                            <div className='flex items-center justify-between w-full'>
-                              <RadioGroup.Label
-                                as='p'
-                                className={`font-semibold fs-16  ${checked ? 'text-white' : 'text-oby-primary'}`}
-                              >
-                                {category.name}
-                              </RadioGroup.Label>
-                              <ChevronRightIcon className={`w-6 h-6  ${checked ? 'text-white' : 'text-oby-primary'}`} />
-                            </div>
-                          )}
-                        </RadioGroup.Option>
-                      )
-                    }
-                  })}
+                            }
+                          >
+                            {({ checked }) => (
+                              <div className='flex items-center justify-between w-full'>
+                                <RadioGroup.Label
+                                  as='p'
+                                  className={`font-semibold fs-16 whitespace-nowrap  ${
+                                    checked ? 'text-white' : 'text-oby-primary'
+                                  }`}
+                                >
+                                  {category.name}
+                                </RadioGroup.Label>
+                                <ChevronRightIcon
+                                  className={`@992:block hidden w-6 h-6  ${
+                                    checked ? 'text-white' : 'text-oby-primary'
+                                  }`}
+                                />
+                              </div>
+                            )}
+                          </RadioGroup.Option>
+                        )
+                      }
+                    })}
+                  </div>
                 </div>
               </RadioGroup>
             </div>
-            <div className='@992:col-span-9 @992:order-2 order-1 col-span-12'>
+            <div className='@992:col-span-9  col-span-1'>
               <div className='grid @992:grid-cols-3 @768:grid-cols-2 @992:gap-10 @768:gap-6'>
                 {initializedBlogs.length > 0 ? (
                   initializedBlogs.map((item) => (
@@ -98,7 +106,7 @@ export default function BlogPage({ categories, initBlogs }: BlogPageProps) {
                         </div>
                         <div className='mt-3'>
                           <div className='flex items-center gap-2'>
-                            <div className='px-1.5 py-0.75 @992:fs-14 fs-12 border border-oby-blue leading-[130%] rounded-2 text-oby-blue max-w-max'>
+                            <div className='px-1.5 py-0.75 @992:fs-14 fs-12 border border-oby-green leading-[130%] rounded-2 text-oby-green max-w-max'>
                               {generateCateNameById(item.category_ids[0])}
                             </div>
                             <span className='fs-14 text-oby-676869'>-</span>
