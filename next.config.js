c357c995af9a729eb320adc755c116b408fc6baa
ngoctienle <ngoctienle.2399@gmail.com>
2024-05-18 12:43:05 +0700

    fix ip

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const isDev = process.env.NODE_ENV === 'development'
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['api.ongbayeu.com', 'api.ongbayeu.comundefined'],
+    domains: ['13.229.247.102undefined', '13.229.247.102'],
     minimumCacheTTL: 86400
   },
   compiler: {
